b1d5cd65252ac17c8451dd355b2fc361f51e9c4e
ICTD <ICTD@cda.ph>
2026-07-21 00:24:04 +0800

    update login filename

--- header.php
+++ header.php
@@ -256,7 +256,7 @@
                     </div>
                 </div>
             <?php else: ?>
-                <a href="<?= $pathPrefix ?>admin/login.php" class="admin-link-btn" title="Admin Login">
+                <a href="<?= $pathPrefix ?>admnin_login.php" class="admin-link-btn" title="Admin Login">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                     <span class="btn-text">Admin Portal</span>
                 </a>
